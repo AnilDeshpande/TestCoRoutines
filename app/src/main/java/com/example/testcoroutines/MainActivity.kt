22b71b66e8567fdd1ce069b2209748d7c2f72c1d
Anil Deshpande <anil.deshpande@shell.com>
2023-03-14 20:18:01 +0530

    initial Commit

--- app/src/main/java/com/example/testcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/testcoroutines/MainActivity.kt
@@ -2,22 +2,20 @@ package com.example.testcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity(), CoroutineScope {
-
-    private var job: Job = Job()
-
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        launch { longRunningWork("SampleCoRoutine",1000) }
+        Log.i(MainActivity.TAG,"Main Activity thread started")
+        longRunningWork("SampleCoRoutine",1000)
+        Log.i(MainActivity.TAG,"Main Activity thread ended")
     }
 
     companion object{
@@ -25,12 +23,14 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     }
 }
 
-suspend fun longRunningWork(coroutineName: String, delay: Long) {
+fun longRunningWork(coroutineName: String, delay: Long) {
     Log.i(MainActivity.TAG,"$coroutineName thread started")
     Log.i(MainActivity.TAG,"Thread name: ${Thread.currentThread().name} Thread id: ${Thread.currentThread().id} in ${coroutineName}")
     for( i in 0..9){
-        Log.i(MainActivity.TAG,"Remaining time left for ${Thread.currentThread().name}:${10 - i} in $coroutineName")
-        delay(delay)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            Log.i(MainActivity.TAG,"Remaining time left for ${Thread.currentThread().name}:${10 - i} in $coroutineName")
+        }, delay)
+
     }
     Log.i(MainActivity.TAG,"$coroutineName thread ended")
 }
